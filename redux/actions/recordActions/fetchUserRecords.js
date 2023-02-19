6e22d8c8f213e7e29580ec85ed6367e78908c2c7
Jake Garcia <bballwiz4@gmail.com>
2023-02-18 18:26:16 -0800

    Added a one second timeout to the fetchUserRecords action so that a loading animation can be shown.

--- redux/actions/recordActions/fetchUserRecords.js
+++ redux/actions/recordActions/fetchUserRecords.js
@@ -6,19 +6,21 @@ const fetchUserRecords = (userId) => {
     let url = `${baseUrl}records/${userId}`;
     return async dispatch => {
         dispatch({type: "FETCHING_RECORDS"});
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                let {success} = data;
-                if (success === true) {
-                    let {userRecords} = data;
-                    return dispatch({type: "RECORDS_FETCH_SUCCESS", userRecords});
-                } else {
-                    let {errors} = data;
-                    let message = errors[0].message;
-                    return dispatch({type: "RECORDS_FETCH_ERROR", errorMessage: message});
-                }
-            })
+        setTimeout(() => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    let {success} = data;
+                    if (success === true) {
+                        let {userRecords} = data;
+                        return dispatch({type: "RECORDS_FETCH_SUCCESS", userRecords});
+                    } else {
+                        let {errors} = data;
+                        let message = errors[0].message;
+                        return dispatch({type: "RECORDS_FETCH_ERROR", errorMessage: message});
+                    }
+                })
+        }, 1000);
     }
 }
 
