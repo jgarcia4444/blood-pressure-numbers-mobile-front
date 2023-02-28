44f144285e30b595a6d0a5868d9e49632ae01a4f
Jake Garcia <bballwiz4@gmail.com>
2023-02-28 15:26:59 -0800

    The print button is valid if the user has all records selected, but if the from or the to date are empty when they select date range then the print button is set back to invalid. Layout has the labels resting on top of the date pickers now.

--- components/forms/PrintForm/index.js
+++ components/forms/PrintForm/index.js
@@ -1,6 +1,9 @@
-import React, {useState} from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Animated, Easing } from 'react-native';
+import {Ionicons} from 'react-native-vector-icons';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 const {height, width} = Dimensions.get('screen');
 
 import globalStyles from '../../../config/styles/globalStyles';
@@ -10,6 +13,13 @@ const PrintForm = () => {
     const filterSelections = ["All", "Date Range"];
     const [filterIndex, setFilterIndex] = useState(0);
     const [printValid, setPrintValid] = useState(false);
+    const [showDateRange, setShowDateRange] = useState(false);
+    const [fromDate, setFromDate] = useState('');
+    const [toDate, setToDate] = useState('');
+    const [showFromPicker, setShowFromPicker] = useState(false);
+    const [showToPicker, setShowToPicker] = useState(false);
+
+    const rowOpacity = useRef(new Animated.Value(0)).current
 
     const handlePrintPress = () => {
         if (printValid === true) {
@@ -25,13 +35,106 @@ const PrintForm = () => {
         return printValid === true ? {borderWidth: 0, backgroundColor: "#fff", ...platformShadow} : {}
     }
 
+    const handleDateChangeFrom = (e, selectedDate) => {
+
+    }
+
+    const handleDateChangeTo = (e, selectedDate) => {
+        
+    }
+
+    const dateRange = (
+        <Animated.View style={[styles.dateRangeRow, {opacity: rowOpacity}]}>
+            <View style={styles.datePickerColumn}>
+                <Text style={styles.datePickerLabel}>From</Text>
+                <View style={styles.datePickerContainer}>
+                    <TouchableOpacity style={styles.datePickerTouchContainer}>
+                        <View style={styles.dateValueContainer}>
+                            <Text style={styles.dateValue}>{fromDate}</Text>
+                        </View>
+                        <Ionicons color={'#fff'} size={24} name='calendar'/>
+                    </TouchableOpacity>
+                </View>
+                {showFromPicker &&
+                    <DateTimePicker 
+                        testID='dateTimePickerFrom'
+                        mode="date"
+                        is24Hour={true}
+                        onChange={handleDateChangeFrom}
+                    />
+                }
+            </View>
+            <View style={styles.datePickerColumn}>
+                <Text style={styles.datePickerLabel}>To</Text>
+                <View style={styles.datePickerContainer}>
+                    <TouchableOpacity style={styles.datePickerTouchContainer}>
+                        <View style={styles.dateValueContainer}>
+                            <Text style={styles.dateValue}>{toDate}</Text>
+                        </View>
+                        <Ionicons color={'#fff'} size={24} name='calendar'/>
+                    </TouchableOpacity>
+                </View>
+                {showToPicker &&
+                    <DateTimePicker 
+                        testID='dateTimePickerTo'
+                        mode="date"
+                        is24Hour={true}
+                        onChange={handleDateChangeTo}
+                    />
+                }
+            </View>
+        </Animated.View>
+    )
+
+    const fadeRowIn = () => {
+        Animated.timing(rowOpacity, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    const fadeRowOut =() => {
+        Animated.timing(rowOpacity, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: true
+        }).start(() => {
+            setShowDateRange(false);
+            setPrintValid(true);
+        })
+    }
+
+    useEffect(() => {
+        if (filterIndex === 1) {
+            setShowDateRange(true);
+            fadeRowIn();
+            if (fromDate === "" || toDate === "") {
+                setPrintValid(false);
+            }
+        } else {
+            fadeRowOut()
+        }
+    }, [filterIndex])
+
     return (
         <View style={styles.printFormContainer}>
             <View style={styles.printFormTopRow}>
                 <Text style={styles.filterSegmentTitle}>Filter</Text>
                 <SegmentedControl 
                     style={styles.filterSegment}
+                    values={filterSelections}
+                    selectedIndex={filterIndex}
+                    onChange={(e) => {
+                        setFilterIndex(e.nativeEvent.selectedSegmentIndex);
+                    }}
+                    tintColor='#f00'
+                    fontStyle={{color: "#fff"}}
+                    activeFontStyle={{color: '#fff'}}
                 />
+                {showDateRange === true &&
+                    dateRange
+                }
             </View>
             <View style={styles.printFormBottomRow}>
                 <TouchableOpacity onPress={handlePrintPress} style={[styles.printButton, activePrintButton()]}>
@@ -43,6 +146,35 @@ const PrintForm = () => {
 }
 
 const styles = StyleSheet.create({
+    datePickerColumn: {
+        width: "50%",
+    },
+    datePickerContainer: {
+        flexDirection: 'row',
+        width: "100%",
+        borderRadius: '50%',
+        backgroundColor: "#f77",
+    },
+    datePickerLabel: {
+        color: '#fff',
+        fontWeight: '900',
+        fontSize: height * 0.02,
+    },
+    dateRangeRow: {
+        width: "100%",
+        flexDirection: 'row',
+        marginTop: height * 0.02,
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+    datePickerTouchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    dateValueContainer: {
+        width: '75%',
+    },
     filterSegment: {
         widht: "100%",
     },
