d1dcafe8a20fe8be4399b9af77c46694ce4f0fa0
Jake Garcia <bballwiz4@gmail.com>
2023-05-02 13:34:22 -0700

    Added in comments to the handleReminderPress to represent the logic needed. High level.

--- screens/AddRecord/index.js
+++ screens/AddRecord/index.js
@@ -11,12 +11,18 @@ const {pageTitle, pageTitleContainer} = globalStyles;
 
 const AddRecord = () => {
 
+    const handleReminderPress = () => {
+        // Check to see if they have reminders already present
+        // IF TRUE: Allow user to update the time of their reminders.
+        // ELSE: Allow the user to set a time for a daily reminder, but once the time is selected you must ask the user for push notification permission.
+    }
+
     return (
         <MainBackgroundContainer>
             <View style={styles.addRecordScreenContainer}>
                 <View style={[pageTitleContainer, {justifyContent: 'space-between'}]}>
                     <Text style={pageTitle}>Add Record</Text>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleAlertsPress}>
                         <Ionicons style={{marginEnd: 5}} name="alarm" size={28} color={"#fff"} />
                     </TouchableOpacity>
                 </View>
