5bd9af637a588293ffbf6988b100c719c77e6430
Jake Garcia <bballwiz4@gmail.com>
2023-05-02 22:53:34 -0700

    Added in a modal to be shown. Modal is a skeleton. Added in the logic to present and dismiss the modal.

--- screens/AddRecord/index.js
+++ screens/AddRecord/index.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions, KeyboardAvoidingView, Platform, Touchable, TouchableOpacity } from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import { View, Text, StyleSheet, Dimensions, Modal, TouchableOpacity } from 'react-native';
 import {Ionicons} from 'react-native-vector-icons';
 
 import MainBackgroundContainer from '../../components/backgrounds';
 import AddRecordForm from '../../components/forms/AddRecordForm';
 import globalStyles from '../../config/styles/globalStyles';
-const {pageTitle, pageTitleContainer} = globalStyles;
+const {pageTitle, pageTitleContainer, platformShadow} = globalStyles;
 
 
 
 const AddRecord = () => {
 
+    const [showNotificationForm, setShowNotificationForm] = useState(false);
+
     const handleReminderPress = () => {
-        // Check to see if they have reminders already present
-        // IF TRUE: Allow user to update the time of their reminders.
-        // ELSE: Allow the user to set a time for a daily reminder, but once the time is selected you must ask the user for push notification permission.
+        setShowNotificationForm(true);  
     }
+
+    const closeButton = (
+        <TouchableOpacity onPress={() => setShowNotificationForm(false)} style={[styles.closeButton, platformShadow]}>
+            <Ionicons name="close" size={32} color={"#fff"} />
+        </TouchableOpacity>
+    )
+
+    const formModal = (
+        <Modal transparent={true} animationType='slide' style={styles.formModal}>
+            <View style={styles.formModalContainer}>
+                <View style={[styles.formContainer, platformShadow]}>
+                    {closeButton}
+                    <View>
+                        <Text style={styles.formTitle}>Set a reminder time!</Text>
+                    </View>
+                    <View>
+
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
 
     return (
         <MainBackgroundContainer>
             <View style={styles.addRecordScreenContainer}>
                 <View style={[pageTitleContainer, {justifyContent: 'space-between'}]}>
                     <Text style={pageTitle}>Add Record</Text>
-                    <TouchableOpacity onPress={handleAlertsPress}>
-                        <Ionicons style={{marginEnd: 5}} name="alarm" size={28} color={"#fff"} />
+                    <TouchableOpacity onPress={handleReminderPress}>
+                        <Ionicons style={{marginEnd: 10}} name="alarm" size={36} color={"#fff"} />
+                        {showNotificationForm === true &&
+                            formModal
+                        }
                     </TouchableOpacity>
                 </View>
                 <AddRecordForm />
@@ -43,6 +68,36 @@ const styles = StyleSheet.create({
         fontSize: 64,
         fontWeight: 'bold',
     },
+    closeButton: {
+        width: 48,
+        height: 48,
+        borderRadius: '50%',
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: '#f00',
+        position: 'absolute',
+        right: -24,
+        top: -24,
+    },
+    formContainer: {
+        backgroundColor: '#f00',
+        padding: height * 0.04,
+        borderRadius: 5,
+    },
+    formModal: {
+        
+    },
+    formModalContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width,
+        height: height,
+    },
+    formTitle: {
+        color: "#fff",
+        fontWeight: "bold",
+        fontSize: height * 0.03,
+    }
 })
 
 export default AddRecord;
