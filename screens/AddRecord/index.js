881b8844f070b9e2ec0007c5f81ab3825b24fa15
Jake Garcia <bballwiz4@gmail.com>
2023-05-03 14:31:40 -0700

    Added in a function that will get the users permissions and if none are found ask for permission.

--- screens/AddRecord/index.js
+++ screens/AddRecord/index.js
@@ -2,18 +2,30 @@ import React, {useState, useRef, useEffect} from 'react';
 import { View, Text, StyleSheet, Dimensions, Modal, TouchableOpacity } from 'react-native';
 import {Ionicons} from 'react-native-vector-icons';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 import MainBackgroundContainer from '../../components/backgrounds';
 import AddRecordForm from '../../components/forms/AddRecordForm';
 import globalStyles from '../../config/styles/globalStyles';
 const {pageTitle, pageTitleContainer, platformShadow} = globalStyles;
 
-
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
 
 const AddRecord = () => {
 
     const [showNotificationForm, setShowNotificationForm] = useState(false);
     const [reminderTime, setReminderTime] = useState(new Date());
+    const notificationListener = useRef();
+    const responseListener = useRef();
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
 
     const handleReminderPress = () => {
         setShowNotificationForm(true);  
@@ -55,7 +67,52 @@ const AddRecord = () => {
                 </View>
             </View>
         </Modal>
-    )
+    );
+
+    const registerForPushNotificationsAsync = async () => {
+        let token;
+        if (Device.isDevice) {
+            const {status: existingStatus} = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const {status} = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+            console.log(token);
+        } else {
+            alert('Failed to get push token for push notification!');
+        }
+        if (Platform.OS === 'android') {
+            Notifications.setNotificationChannelAsync('default', {
+              name: 'default',
+              importance: Notifications.AndroidImportance.MAX,
+              vibrationPattern: [0, 250, 250, 250],
+              lightColor: '#FF231F7C',
+            });
+        }
+        return token;
+    }
+
+    useEffect(() => {
+        Notifications.r
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+        });
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        }
+    })
 
     return (
         <MainBackgroundContainer>
